2706fbf92924c6e7afafa478457f7bbed9921b43
Blake Griffith <blake.a.griffith@gmail.com>
2025-06-11 12:31:32 -0400

    wip noise handshake test

--- hdht/tests/testnet.rs
+++ hdht/tests/testnet.rs
@@ -1,15 +1,18 @@
 #![allow(unreachable_code)]
 mod common;
+use compact_encoding::CompactEncoding;
 use std::net::SocketAddr;
 
-use async_udx::UdxSocket;
 use common::{js::make_repl, Result};
 use dht_rpc::DhtConfig;
-use futures::{SinkExt, StreamExt};
-use hypercore_protocol::encrypted_framed_message_channel;
-use hyperdht::{HyperDht, HyperDhtEvent, Keypair};
-use rusty_nodejs_repl::{integration_utils::log, wait, Repl};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use futures::StreamExt;
+use hypercore_protocol::{handshake_constants::DHT_PATTERN, HandshakeConfig};
+use hyperdht::{
+    cenc::{NoisePayload, UdxInfo},
+    namespace::PEER_HANDSHAKE,
+    HyperDht, HyperDhtEvent, Keypair,
+};
+use rusty_nodejs_repl::Repl;
 
 #[allow(unused)]
 fn show_bytes<T: AsRef<[u8]>>(x: T) {
@@ -269,13 +272,73 @@ await server.listen(server_node.defaultKeyPair);
     Ok(())
 }
 
+#[tokio::test]
+async fn check_noise() -> Result<()> {
+    let (mut tn, hdht) = setup_rs_node_and_js_testnet!();
+    let pub_key: [u8; 32] = tn
+        .repl
+        .json_run(
+            "
+server_node = testnet.nodes[testnet.nodes.length - 1];
+server = server_node.createServer();
+writeJson([...server_node.defaultKeyPair.publicKey]);
+await server.listen(server_node.defaultKeyPair);
+    ",
+        )
+        .await?;
+    let SocketAddr::V4(rs_client_socket) = hdht.local_addr()? else {
+        panic!();
+    };
+
+    let np = NoisePayload {
+        version: 1,
+        error: 0,
+        firewall: 0,
+        holepunch: None,
+        addresses4: Some(vec![rs_client_socket]),
+        addresses6: None,
+        udx: Some(UdxInfo {
+            version: 1,
+            reusable_socket: true,
+            id: 1,
+            seq: 0,
+        }),
+        secret_stream: None,
+        relay_through: None,
+    };
+    let np_bytes = np.to_encoded_bytes()?;
+    let hc = HandshakeConfig {
+        pattern: DHT_PATTERN,
+        prologue: Some(PEER_HANDSHAKE.to_vec()),
+        remote_public_key: Some(pub_key),
+    };
+
+    let mut hphs = hypercore_protocol::Handshake::new_with_config(true, &hc)?;
+    hphs.set_payload(np_bytes.to_vec());
+    let noise_payload = hphs.start_raw()?.unwrap();
+    let js_payload = serde_json::to_string(&noise_payload)?;
+    let res = tn
+        .repl
+        .str_run(format!(
+            "
+noise = {js_payload};
+handshake = server.createHandshake(server._keyPair, null);
+// this will error if it can't be verified
+res = handshake.recv(Buffer.from(noise));
+writeJson(res);
+"
+        ))
+        .await?;
+    assert!(res.matches("\"version\":1").next().is_some());
+    assert!(res.matches("\"relayThrough\":null").next().is_some());
+    Ok(())
+}
+/*
 /// In Rust create a "Server" and do a "listen" on `public_key`
 /// Then in JavaScript do a `dht.findPeer(public_key)`.
 /// And verify it finds the rust Server
 #[tokio::test]
 async fn js_server_rs_connects() -> Result<()> {
-    use async_compat::CompatExt;
-
     let (mut tn, mut hdht) = setup_rs_node_and_js_testnet!();
 
     // with js announc on topic with the node's default keypair
