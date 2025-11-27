1935f761c65eb4f6b594e35e492584655155c8e8
Blake Griffith <git@cowlicks.website>
2025-11-27 16:42:55 -0600

    Add find_peer

--- hdht/src/adht.rs
+++ hdht/src/adht.rs
@@ -7,11 +7,13 @@ use std::{
 };
 
 use compact_encoding::CompactEncoding;
-use dht_rpc::{commit::Commit, io::InResponse, AsyncRpcDht, DhtConfig, IdBytes, QueryNext};
+use dht_rpc::{
+    cenc::generic_hash, commit::Commit, io::InResponse, AsyncRpcDht, DhtConfig, IdBytes, QueryNext,
+};
 use futures::Stream;
 use tracing::instrument;
 
-use crate::{commands, queries::QueryResult, Result, DEFAULT_BOOTSTRAP};
+use crate::{commands, crypto::PublicKey, queries::QueryResult, Result, DEFAULT_BOOTSTRAP};
 
 pub struct Dht {
     rpc: AsyncRpcDht,
@@ -32,6 +34,11 @@ impl Dht {
         })
     }
 
+    pub async fn boostrap(&self) -> Result<()> {
+        self.rpc.bootstrap().await?;
+        Ok(())
+    }
+
     pub fn lookup(&self, target: IdBytes, commit: Commit) -> Result<Lookup> {
         let query = self.rpc.query_next(commands::LOOKUP, target, None, commit);
         Ok(Lookup {
@@ -39,6 +46,38 @@ impl Dht {
             topic: target,
             collected_responses: Vec::new(),
         })
+    }
+
+    pub fn find_peer(&self, pub_key: PublicKey) -> Result<FindPeer> {
+        let target = IdBytes(generic_hash(&*pub_key));
+        let query = self
+            .rpc
+            .query_next(commands::FIND_PEER, target, None, Commit::No);
+        Ok(FindPeer {
+            query,
+            topic: target,
+            collected_responses: Vec::new(),
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct LookupResponse {
+    pub response: Arc<InResponse>,
+    pub peers: Vec<crate::cenc::Peer>,
+}
+
+impl LookupResponse {
+    #[instrument(skip_all, err)]
+    fn decode_response(response: Arc<InResponse>) -> Result<Option<LookupResponse>> {
+        let Some(value) = &response.response.value else {
+            return Ok(None);
+        };
+        println!("decode_response_value {value:?}");
+        let (peers, _rest): (Vec<crate::cenc::Peer>, &[u8]) =
+            <Vec<crate::cenc::Peer> as CompactEncoding>::decode(value)?;
+        debug_assert!(_rest.is_empty());
+        Ok(Some(LookupResponse { response, peers }))
     }
 }
 
@@ -49,21 +88,15 @@ pub struct Lookup {
     collected_responses: Vec<Arc<InResponse>>,
 }
 
-#[derive(Debug)]
-pub struct PeersResponse {
-    pub response: Arc<InResponse>,
-    pub peers: Vec<crate::cenc::Peer>,
-}
-
 impl Stream for Lookup {
-    type Item = Result<Option<PeersResponse>>;
+    type Item = Result<Option<LookupResponse>>;
 
     #[instrument(skip_all)]
     fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         Pin::new(&mut self.query).poll_next(cx).map(|x| {
             x.map(|response| {
                 self.collected_responses.push(response.clone());
-                PeersResponse::decode_response(response)
+                LookupResponse::decode_response(response)
             })
         })
     }
@@ -93,18 +126,66 @@ impl Future for Lookup {
     }
 }
 
-impl PeersResponse {
-    /// `Ok(Some(Self))` if the response was successfully decoded,
-    /// `Ok(None)` if the response doesn't have a value field,
-    /// `Err(e)` if there was an error decoding.
+#[derive(Debug)]
+pub struct FindPeerResponse {
+    pub response: Arc<InResponse>,
+    pub peer: crate::cenc::Peer,
+}
+
+impl FindPeerResponse {
     #[instrument(skip_all, err)]
-    fn decode_response(response: Arc<InResponse>) -> Result<Option<PeersResponse>> {
+    fn decode_response(response: Arc<InResponse>) -> Result<Option<FindPeerResponse>> {
         let Some(value) = &response.response.value else {
             return Ok(None);
         };
-        let (peers, _rest): (Vec<crate::cenc::Peer>, &[u8]) =
-            <Vec<crate::cenc::Peer> as CompactEncoding>::decode(value)?;
+        println!("decode_response_value {value:?}");
+        let (peer, _rest): (crate::cenc::Peer, &[u8]) =
+            <crate::cenc::Peer as CompactEncoding>::decode(value)?;
         debug_assert!(_rest.is_empty());
-        Ok(Some(PeersResponse { response, peers }))
+        Ok(Some(FindPeerResponse { response, peer }))
+    }
+}
+
+pub struct FindPeer {
+    query: QueryNext,
+    topic: IdBytes,
+    collected_responses: Vec<Arc<InResponse>>,
+}
+
+impl Stream for FindPeer {
+    type Item = Result<Option<FindPeerResponse>>;
+
+    #[instrument(skip_all)]
+    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        Pin::new(&mut self.query).poll_next(cx).map(|x| {
+            x.map(|response| {
+                self.collected_responses.push(response.clone());
+                FindPeerResponse::decode_response(response)
+            })
+        })
+    }
+}
+
+impl Future for FindPeer {
+    type Output = Result<QueryResult>;
+
+    #[instrument(skip_all)]
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        loop {
+            match Pin::new(&mut self.query).poll_next(cx) {
+                Poll::Ready(Some(response)) => self.collected_responses.push(response),
+                Poll::Ready(None) => break,
+                Poll::Pending => return Poll::Pending,
+            }
+        }
+        match Pin::new(&mut self.query).poll(cx) {
+            Poll::Ready(Ok(query_result)) => Poll::Ready(Ok(QueryResult {
+                topic: self.topic,
+                responses: std::mem::take(&mut self.collected_responses),
+                query_id: query_result.query_id,
+            })),
+            Poll::Ready(Err(e)) => Poll::Ready(Err(e.into())),
+            Poll::Pending => Poll::Pending,
+        }
     }
 }
