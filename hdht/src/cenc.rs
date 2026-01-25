c2ffa459af9912453e31c75c7fa58a75eee4cd6f
Blake Griffith <git@cowlicks.website>
2026-01-25 00:57:25 -0500

    log when we get a wrong payload version

--- hdht/src/cenc.rs
+++ hdht/src/cenc.rs
@@ -7,6 +7,7 @@ use compact_encoding::{
     take_array, vec_encoded_size_for_fixed_sized_elements, write_array,
 };
 use std::net::{SocketAddrV4, SocketAddrV6};
+use tracing::error;
 
 const UDX_INFO_VERSION: usize = 1;
 const UDX_INFO_DEFAULT_SEQ: usize = 0;
@@ -593,6 +594,9 @@ impl CompactEncoding for NoisePayload {
         // error. But maybe not a kind we currently have in compact_encoding. Maybe a new:
         // IncompatibleVersion error should be added.
         if version != NOISE_PAYLOAD_VERSION {
+            error!(
+                "Received unexpected NOISE_PAYLOAD_VERSION = [{version}]. We expected [{NOISE_PAYLOAD_VERSION}]"
+            );
             return Ok((
                 Self {
                     version,
